db8bbfd6d0d6b63d072dbedccca7a8c3045362d5
Paul <leprotb76@gmail.com>
2023-01-26 18:02:08 +0100

    V1.0.0

---
+++ config/config.copy.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+  Prefix: "?", // INUTILISÉ
+
+  Users: {
+    OWNERS: ["aa"] // INUTILISÉ
+  },
+
+  Client: {
+    TOKEN: "", // Token du bot^^
+    ID: "" // ID du Bot
+  },
+
+  Bdd: {
+    host: "", // IP de connexion
+    password: "",
+    user: "",
+    port: "3306",
+    database: "",
+  },
+
+  Divers: {
+    activité: "Her-Network",
+    version: "1.0",
+  }
+
+}
